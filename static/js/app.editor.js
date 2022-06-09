e237273b082fb0f61cf8b732693da1688e25d0a1
chairod <chairod.p@live.com>
2022-06-09 10:07:23 +0700

    ปรับการแสดง Resize Eraser & Map Property Smoothy

--- static/js/app.editor.js
+++ static/js/app.editor.js
@@ -941,7 +941,7 @@ angular.module('AppEditor', []).controller('EditorController', ($scope, $timeout
     $scope.toggleMapProperty = (enable, immediate) => {
         $timeout.cancel(toggleMapPropertyTimeoutId);
         if(!$scope.$settings.map.isReady) return;
-        
+
         if(!enable){
             if($scope.$settings.formView.resizingMapProperty) return;
             if(immediate){
@@ -1050,8 +1050,8 @@ angular.module('AppEditor', []).controller('EditorController', ($scope, $timeout
 }).run(($rootScope) => {
     $rootScope.$appSetting = {
         api: {
-            baseRoute: 'http://localhost:3000/api/map-editor'
-            //baseRoute: 'https://fw-map-editor.herokuapp.com/api/map-editor'
+            //baseRoute: 'http://localhost:3000/api/map-editor'
+            baseRoute: 'https://fw-map-editor.herokuapp.com/api/map-editor'
         }
     };
 }).service('$fileReaderService', function ($q, $dialogService) {
